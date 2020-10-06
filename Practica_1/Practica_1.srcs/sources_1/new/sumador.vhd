c0378de1e1564bf354690bbf22adcd376e8cd986
A.L.K <alejandrobarrachina.02@gmail.com>
2020-10-06 13:54:33 +0200

    Practica 1 added

---
+++ Practica_1/Practica_1.srcs/sources_1/new/sumador.vhd
@@ -0,0 +1,22 @@
+--=============================
+-- Alejandro Barrachina Argudo
+-- Práctica 1: parte básica
+-- Sumador
+--=============================
+
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL :
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+ENTITY sum IS
+    PORT (
+        A : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+        B : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+        C : OUT STD_LOGIC_VECTOR(3 DOWNTO 0));
+END sum;
+
+ARCHITECTURE Behavioral OF sum IS
+BEGIN
+    C <= A + B;
+END Behavioral; -- Behavioral
